1a0ab93b78e4ba71586afabed5151baf977cf059
yara <yara@aus.edu>
2024-02-01 15:04:04 +0400

    Feature B added

--- src/pack1/example.java
+++ src/pack1/example.java
@@ -5,6 +5,7 @@ public class example {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello wORLD");
+		System.out.println("Feature B");
 
 	}
 
